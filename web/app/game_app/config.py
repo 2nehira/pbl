bec0d53d24164045bafc232b727a4a2494c709d7
tunehira <kk.oak6@gmail.com>
2020-01-17 17:41:05 +0900

    second commit

--- web/app/game_app/config.py
+++ web/app/game_app/config.py
@@ -1,6 +1,6 @@
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@db:3306/pbl'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@db:3306/pbl'
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@172.17.0.2:3306/pbl'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tunehira@localhost:3306/pbl'
-
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@172.17.0.2:3306/pbl'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tunehira@localhost:3306/pbl'
 SECRET_KEY = 'secret key'
